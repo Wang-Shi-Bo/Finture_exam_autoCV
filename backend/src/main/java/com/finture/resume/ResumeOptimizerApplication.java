edcadaf650127f8c61a10cf1d3ccdd157d301934
Mata <Mrwangshibo@163.com>
2026-06-02 23:25:21 +0800

    feat: init Spring Boot project with dependencies

---
+++ backend/src/main/java/com/finture/resume/ResumeOptimizerApplication.java
@@ -0,0 +1,11 @@
+package com.finture.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeOptimizerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeOptimizerApplication.class, args);
+    }
+}
